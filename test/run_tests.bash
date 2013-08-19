25f3268d3d10d8710b395c80c46a059bfdd9e775
Joshua Neuheisel <joshua@neuheisel.us>
2013-08-18 20:57:27 -0400

    Added first basic non-trivial test.

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -42,6 +42,19 @@ function test_simple {
 	rm -rf $TESTDIR
 }
 
+function test_basic {
+	local TESTDIR="$BASEDIR/test_simple"
+	mkdir -p $TESTDIR
+	cp Makefile_basic $TESTDIR/Makefile
+	cp func_basic.c $TESTDIR/func.c
+	cp main_basic.c $TESTDIR/main.c
+	(cd $TESTDIR && ../../neumake 1>/dev/null 2>&1)
+	(cd $TESTDIR && ../../neumake clean 1>/dev/null 2>&1)
+	(cd $TESTDIR && ../../neumake all 1>/dev/null 2>&1)
+	(cd $TESTDIR && ../../neumake clean 1>/dev/null 2>&1)
+	rm -rf $TESTDIR
+}
+
 #
 # Run them!
 #
@@ -49,6 +62,7 @@ function test_simple {
 test_no_makefile
 test_empty
 test_simple
+test_basic
 
 #
 # Clean up
